5409a973f607251e3698b700230c77f711a3263c
jakefoglia <53474414+jakefoglia@users.noreply.github.com>
2021-04-02 17:52:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,27 +11,27 @@ using namespace Magick;
 
 int main(int argc,char **argv) 
 { 
-  printf("HELLO WORLD!!!\n");
-  InitializeMagick(*argv);
+    printf("Hello world\n");
+    InitializeMagick(*argv);
 
-  // Construct the image object. Seperating image construction from the 
-  // the read operation ensures that a failure to read the image file 
-  // doesn't render the image object useless. 
-  Image image;
-  try { 
-    // Read a file into image object 
-    image.read( "logo:" );
+    // Construct the image object. Seperating image construction from the 
+    // the read operation ensures that a failure to read the image file 
+    // doesn't render the image object useless. 
+    Image image;
+    try { 
+        // Read a file into image object 
+        image.read( "logo:" );
 
-    // Crop the image to specified size (width, height, xOffset, yOffset)
-    image.crop( Geometry(100,100, 100, 100) );
+        // Crop the image to specified size (width, height, xOffset, yOffset)
+        image.crop( Geometry(100,100, 100, 100) );
 
-    // Write the image to a file 
-    image.write( "logo.png" ); 
-  } 
-  catch( Exception &error_ ) 
-    { 
-      cout << "Caught exception: " << error_.what() << endl; 
-      return 1; 
+        // Write the image to a file 
+        image.write( "logo.png" ); 
     } 
-  return 0; 
+    catch( Exception &error_ ) 
+    { 
+        cout << "Caught exception: " << error_.what() << endl; 
+        return 1; 
+    } 
+    return 0; 
 }
